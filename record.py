2bf59235a14f54ff2b6a337ec88cfb778aca8715
Azril Nazli Alias <azril.nazli@gmail.com>
2022-11-12 11:41:45 +0800

    Naming convention

--- record.py
+++ record.py
@@ -4,10 +4,23 @@ Webcam device listed by vfwcap
 RAM limited to 2GB
 Output video name is output.mkv
 """
+import os
 import subprocess
+from datetime import datetime
+
+# Get the current date and
+# time from system
+# and use strftime function
+# to format the date and time.
+#curr_datetime = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+curr_datetime = datetime.now().strftime('%Y%m%d%H%M%S')
 
 # FFMPEG command
-command = "ffmpeg -hide_banner -y -f vfwcap -t 10 -re -rtbufsize 2147.48M -i 0 videos/out.mkv"
+command = "ffmpeg -hide_banner -y -f vfwcap -t 10 -re -rtbufsize 2147.48M -i 0 videos/%s_%s.mkv" % ('CAM001', curr_datetime)
 
+#print(command)
 # Run the command
 subprocess.run(command)
+
+# Convert the recorded video to H264 Video Codec
+#command = "ffmpeg -i videos/out.mkv"
