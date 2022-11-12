1212fce65193b34c6a2b736c97ca72c3ce41d516
Azril Nazli Alias <azril.nazli@gmail.com>
2022-11-12 11:26:46 +0800

    Add .gitignore and change output path

--- record.py
+++ record.py
@@ -7,7 +7,7 @@ Output video name is output.mkv
 import subprocess
 
 # FFMPEG command
-command = "ffmpeg -y -f vfwcap -t 10 -re -rtbufsize 2147.48M -i 0 out.mkv"
+command = "ffmpeg -hide_banner -y -f vfwcap -t 10 -re -rtbufsize 2147.48M -i 0 videos/out.mkv"
 
 # Run the command
 subprocess.run(command)
